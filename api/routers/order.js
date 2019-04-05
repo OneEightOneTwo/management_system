20a3916813bdae0d9119d1aa76a88945fa087d02
czhihua <13927976266@163.com>
2019-04-05 23:56:59 +0800

    订单页修改

--- api/routers/order.js
+++ api/routers/order.js
@@ -6,10 +6,44 @@ var router = new Router();
 
 router.post('/', async (ctx, next) => {
     //获取前端传过来的数据
-    let { table, query, page, num } = ctx.request.body;
-    let initial = await db.findRestrict(table, query, page, num);
-    // ctx.body = ctx.request.body;
-    ctx.body = initial;
+    let { m } = ctx.request.body;
+
+    if (m == 'findPortion') {
+        let { table, query, page, num } = ctx.request.body;
+        let res = await db.findRestrict(table, query, page, num);
+        // ctx.body = ctx.request.body;
+        ctx.body = res;
+    }
+    if (m == 'del') {
+        let { table, query } = ctx.request.body;
+        let res = await db.del(table, { 'ordernum': query * 1 });
+        ctx.body = res;
+    }
+    // if (m == 'find') {
+    //     let { table, query } = ctx.request.body;
+    //     let queryarr = query;
+    //     let arr1 = [];//赋值条件
+    //     for (let i = 0; i < queryarr.length; i++) {
+    //         //判断数值类型
+    //         let str = query[i];
+    //         let arr = str.split(':');//让字符串以‘:’分隔成数组
+    //         attr1 = arr[0];
+    //         attr2 = arr[1];
+    //         if (attr1 == 'start') {
+    //             arr1.push({ 'order_time': { $gt: attr2 * 1 } });
+    //         }
+    //         if (attr1 == 'end') {
+    //             arr1.push({ 'order_time': { $lt: attr2 * 1 } });
+    //         }
+    //         if (attr1 != 'start' && attr1 != 'end') {
+    //             arr1.push({ attr1: attr2 });
+    //         }
+    //     }
+    //     let res = await db.find(table, { 'goodsnum': 3 });
+    //     ctx.body = arr1;
+    // }
+
+
 });
 
 module.exports = router;
--- js/order.js
+++ js/order.js
@@ -5,16 +5,19 @@ $(function () {
         type: 'post',
         url: '/order',
         data: {
+            m: 'findPortion',
             table: 'order',
             query: '',
             page: 1,
             num: 10
         },
         success: function (str) {
+            // console.log(str);
+
             let html = str.map((item) => {
                 return `<tr>
                         <td>
-                            <div class="layui-unselect layui-form-checkbox" lay-skin="primary" data-id='2'>
+                            <div class="layui-unselect layui-form-checkbox" lay-skin="primary" data-id='${item.ordernum}'>
                             <i class="layui-icon">&#xe605;</i>
                             </div>
                         </td>
@@ -32,7 +35,7 @@ $(function () {
                             <a title="查看" onclick="x_admin_show('编辑','order-view.html')" href="javascript:;">
                             <i class="layui-icon">&#xe63c;</i>
                             </a>
-                            <a title="删除" onclick="member_del(this,'要删除的id')" href="javascript:;">
+                            <a title="删除" onclick="member_del(this,${item.ordernum})" href="javascript:;">
                             <i class="layui-icon">&#xe640;</i>
                             </a>
                         </td>
@@ -41,4 +44,61 @@ $(function () {
             $('#order_table tbody').html(html);
         }
     });
+
+    $('#sreach1').click(function () {
+        // let aaa = [{ 'aaaaa': 'aaaa' }, { 'aaaaa': 'aaaa' }, { 'aaaaa': 'aaaa' }];
+        // console.log(aaa);
+        // let arr = [];
+        // let start = $('#start').val();
+        // let end = $('#end').val();
+        // let order_status = $('.order_status').val();
+        // let pay_way = $('.pay_way').val();
+        // let ordernum = $('#ordernum').val();
+        // let shipping_status = $('.shipping_status').val();
+        // if (start) {
+        //     arr.push('start:' + start);
+        // }
+        // if (end) {
+        //     arr.push('end:' + end);
+        // }
+        // if (order_status) {
+        //     if (order_status != '订单状态') {
+        //         arr.push('order_status:' + order_status);
+        //     }
+        // }
+        // if (pay_way) {
+        //     if (pay_way != '支付方式') {
+        //         arr.push('pay_way:' + pay_way);
+        //     }
+        // }
+        // if (ordernum) {
+        //     arr.push('order_status:' + ordernum);
+        // }
+        // if (shipping_status) {
+        //     if (shipping_status != '发货状态') {
+        //         arr.push('shipping_status:' + shipping_status);
+        //     }
+
+        // }
+
+        // if (arr.length < 0) {
+        //     $.ajax({
+        //         type: 'post',
+        //         url: '/order',
+        //         data: {
+        //             m: 'find',
+        //             table: 'order',
+        //             query: arr,
+        //         },
+        //         success: function (str) {
+
+        //             console.log(str);
+        //         }
+        //     });
+        // }
+
+
+
+    });
+
 });
